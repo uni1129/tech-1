9079f5398208ab2e04c2bdb5fcc5c71613786efe
uni1129 <risajm.0307@icloud.com>
2025-03-13 13:27:54 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+//基礎編
+//1
+console.log('HelloWorld');
+
+//2
+console.log(3 + 7);
+
+//3
+console.log(3 + '7');
+
+//4
+console.log(5 * 2);
+
+//5
+console.log('プログラミング' + 'スクール');
+
+//6
+let name;
+name = 'プログラミングスクール';
+
+console.log(name);
+
+//7
+let a;
+a = 100;
+
+console.log(a * a);
+
+//8
+let box;
+box = 'プログラム初心者';
+
+console.log(box);
+
+box = 'プログラミング中級者';
+
+console.log(box);
+
+//9
+const language = '日本語';
+
+console.log('${ language }を話せます');
+
